d83bf868d50d79ecb4756fc5bc5d65bf585deea7
zayn7232 <56922832+zayn7232@users.noreply.github.com>
2022-06-22 22:56:11 +0530

    Create screenshot.py

---
+++ screenshot.py
@@ -0,0 +1,10 @@
+import pyscreenshot
+  
+# To capture the screen
+image = pyscreenshot.grab()
+  
+# To display the captured screenshot
+image.show()
+  
+# To save the screenshot
+image.save("demo.png")
